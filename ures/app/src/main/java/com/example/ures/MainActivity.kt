af73d84a13fe7fc35334221887d5f364f136f9e4
Tonesz <32124025+Tonesz@users.noreply.github.com>
2020-02-09 10:13:21 +0100

    Add files via upload

---
+++ ures/app/src/main/java/com/example/ures/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ures
+
+import android.os.Bundle
+import android.support.v4.app.AppCombatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+
+
+}
